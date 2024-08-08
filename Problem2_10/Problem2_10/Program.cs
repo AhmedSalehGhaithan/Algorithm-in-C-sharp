1fc4d75ba8c066b601bb5d79fb86193a2166b0e3
Ahmed ghaithan <ghaithanahmed691@gmail.com>
2024-08-08 03:13:33 +0300

    Add files via upload

---
+++ Problem2_10/Problem2_10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Pronlem2_10
+{
+
+    static int GetInputNumber()
+    {
+        int number = 1;
+        do
+        {
+            Console.WriteLine("Enter the number");
+            number = int.Parse(Console.ReadLine());
+        } while (number <= 0);
+
+        return number;
+    }
+
+    static int ReverseNumber(int number)
+    {
+        int reminder = 0;
+        int number2 = 0;
+        while (number > 0)
+        {
+            reminder = number % 10;
+            number = number / 10;
+            number2 = number2 * 10 + reminder;
+        }
+        return number2;
+    }
+
+    static void PrintDigit(int number)
+    {
+        int reminder = 0;
+        while (number > 0)
+        {
+            reminder = number % 10;
+            number = number / 10;
+            Console.WriteLine(reminder);
+        }
+    }
+    
+    
+    static void Main(string[] args)
+    {
+        PrintDigit(ReverseNumber(GetInputNumber()));
+    }
+}
